c48a83c0facf5333855bd4a694c80991c0eb1ece
danielhuang-030 <danielhuang030@gmail.com>
2026-09-23 21:51:01 +0800

    chore: bump version to 0.2.6

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PLUGIN_ID=codex-selective-ping
-VERSION=0.2.5
+VERSION=0.2.6
 
 .PHONY: test build-linux clean verify-product verify-version
 
--- registration.go
+++ registration.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	pluginName = "codex-selective-ping"
-	version    = "0.2.5"
+	version    = "0.2.6"
 )
 
 func configFields(cfg config.Config) []map[string]any {
